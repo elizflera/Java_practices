71f7a305c0c8370ff7711391cd25ae59f59a5279
Elizaveta <elizavetaflera@gmail.com>
2021-10-25 14:21:28 +0300

    First commit

---
+++ src/ru/mirea/practice/practice1/Book.java
@@ -0,0 +1,23 @@
+package ru.mirea.practice.practice1;
+import java.lang.*;
+public class Book {
+    private String author;
+    private String name;
+    private int page;
+
+    public Book(String a, String n, int p) {
+        this.author = a;
+        this.name = n;
+        this.page = p;
+    }
+
+    public void setBook(String a, String n, int p) {
+        this.author = a;
+        this.name = n;
+        this.page = p;
+    }
+
+    public String toString() {
+        return author + " " + name + " has " + page + " pages";
+    }
+}
